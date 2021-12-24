74c70c638b4312a0212413fb10d2b51bbdbaa4df
HProsperouS <96221600+HProsperouS@users.noreply.github.com>
2021-12-24 19:14:22 +0800

    。

--- shop/EditProduct.py
+++ shop/EditProduct.py
@@ -1,5 +1,6 @@
 from flask_uploads import UploadSet, IMAGES
-from wtforms import Form, BooleanField, TextAreaField
+from wtforms import Form, TextAreaField, StringField
+from wtforms.fields.core import DateField, IntegerField,FloatField,DateField
 #StringField, RadioField, SelectField, validators
 from wtforms.validators import DataRequired
 from flask_wtf.file import FileField, FileRequired
@@ -8,12 +9,12 @@ photos = UploadSet('photos', IMAGES)
 
 class UpdateProductForm(Form):
     product_image = FileField('Product Image',validators=[FileRequired()])
-    produt_name = TextAreaField('Product Name', validators=[DataRequired()])
-    product_category = TextAreaField('Product Category', validators=[DataRequired()])
-    product_price= TextAreaField('Product Price', validators=[DataRequired()])
+    produt_name = StringField('Product Name', validators=[DataRequired()])
+    product_category = StringField('Product Category', validators=[DataRequired()])
+    product_price= FloatField('Product Price', validators=[DataRequired()])
     product_description= TextAreaField('Product Description', validators=[DataRequired()])
-    product_stock= TextAreaField('Product Stock', validators=[DataRequired()])
-    
+    product_stock= IntegerField('Product Stock', validators=[DataRequired()])
+    create_time = DateField('Date Joined', format='%Y-%m-%d')
 
 #Form object – Used to create a WTForms Form.
 #StringField object – Used to create an HTML textfield.
